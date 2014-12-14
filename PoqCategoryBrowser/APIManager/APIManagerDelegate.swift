40b8d27e357b97fa9b706c19769270e9721a650a
Zoran Plesko <zoran.plesko@gmail.com>
2014-12-14 19:34:51 +0100

    Removed submodule Alamofire

---
+++ PoqCategoryBrowser/APIManager/APIManagerDelegate.swift
@@ -0,0 +1,13 @@
+//
+//  APIManagerDelegate.swift
+//  PoqCategoryBrowser
+//
+//  Created by Zoran Plesko on 13/12/14.
+//  Copyright (c) 2014 Masinerija. All rights reserved.
+//
+
+import UIKit
+
+class APIManagerDelegate: NSObject {
+   
+}
---
+++ PoqCategoryBrowser/Model/PoqCategoryModel.swift
@@ -0,0 +1,13 @@
+//
+//  PoqCategoryModel.swift
+//  PoqCategoryBrowser
+//
+//  Created by Zoran Plesko on 13/12/14.
+//  Copyright (c) 2014 Masinerija. All rights reserved.
+//
+
+import UIKit
+
+class PoqCategoryModel: NSObject {
+   
+}
